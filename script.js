ad638896f24cb58a98c6abfa03c0b436103046ad
Tchain Group <78317339+TchainGroup@users.noreply.github.com>
2021-05-22 14:36:52 -0300

    Primeiro commit dos files

---
+++ script.js
@@ -0,0 +1,9 @@
+const receita = document.getElementById("receita");
+
+receita.addEventListener("click",
+    ()=>{
+        receita.style.animation ="hover 0.5s ease-out forwards"
+        console.log('oi')
+    }
+)
+
